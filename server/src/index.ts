b473309c23884d08b1ecabb6589270186e59c32d
imaimai <onakasuitanaapple@gmail.com>
2025-05-13 20:57:29 +0900

    post api

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,11 +4,26 @@ import { PrismaClient } from "./generated/prisma";
 
 const app = express();
 const PORT = 8080;
-
+app.use(express.json());
 const prisma = new PrismaClient();
 
 app.get("/alltodos", async(req:Request,res:Response)  => {
   const allTodos = await prisma.todo.findMany();
   return res.json(allTodos);
 })
+
+app.post("/createtodo", async(req:Request,res:Response)  => {
+  try{
+  const {title, isCompleted} = req.body;
+  const createTodo = await prisma.todo.create({
+    data:{
+      title,
+      isCompleted,
+    },
+  });
+  return res.json(createTodo);
+} catch (e){
+  return e;
+}
+});
 app.listen(PORT,()=> console.log("server is running"));
