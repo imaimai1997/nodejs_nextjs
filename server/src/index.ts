acb0e9822e3f50b167aaa5ef789c95d28f40bbf3
imaimai <onakasuitanaapple@gmail.com>
2025-05-13 20:45:37 +0900

    prisma api try

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import type { Request, Response} from "express";
+import { PrismaClient } from "./generated/prisma"; 
 
 const app = express();
 const PORT = 8080;
 
-app.get("/apptodos", (req:Request,res:Response)  => {
-  return res.send("todos");
+const prisma = new PrismaClient();
+
+app.get("/alltodos", async(req:Request,res:Response)  => {
+  const allTodos = await prisma.todo.findMany();
+  return res.json(allTodos);
 })
 app.listen(PORT,()=> console.log("server is running"));
